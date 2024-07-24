1ea302ce2d70eb2b254eb9323269c043005804cb
Lord Turmoil <tony-turmoil@outlook.com>
2024-07-24 10:37:17 +0800

    update: add updatedAt in update

--- src/main/java/cn/edu/buaa/patpat/boot/modules/discussion/dto/DiscussionUpdateDto.java
+++ src/main/java/cn/edu/buaa/patpat/boot/modules/discussion/dto/DiscussionUpdateDto.java
@@ -1,9 +1,12 @@
 package cn.edu.buaa.patpat.boot.modules.discussion.dto;
 
+import cn.edu.buaa.patpat.boot.common.models.HasUpdated;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
-public class DiscussionUpdateDto {
+@EqualsAndHashCode(callSuper = true)
+public class DiscussionUpdateDto extends HasUpdated {
     private int id;
     private int type;
     private String title;
