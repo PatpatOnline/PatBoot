3634b1932c6194fe4f00eff3b2c94afc4272334c
Lord Turmoil <tony-turmoil@outlook.com>
2024-07-30 23:21:19 +0800

    fix: PageListDto field order

--- src/main/java/cn/edu/buaa/patpat/boot/common/dto/PageListDto.java
+++ src/main/java/cn/edu/buaa/patpat/boot/common/dto/PageListDto.java
@@ -13,9 +13,9 @@ import java.util.List;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class PageListDto<TData> {
     private final List<TData> items;
+    private final int total;
     private final int page;
     private final int pageSize;
-    private final int total;
 
     public static <TData> PageListDto<TData> of(List<TData> items, int total, int page, int pageSize) {
         return new PageListDto<>(items, total, page, pageSize);
